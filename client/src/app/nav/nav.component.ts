4935e94de240dae6f03fdc989c4113c44887dde4
Christian Ofori-Achiaw <christianoforiachiaw@gmail.com>
2020-12-14 13:38:23 +0000

    Add files via upload

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -13,20 +13,15 @@ import { MessageService } from '../_services/message.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  container = 'Unread';
-  messages: Message[];
-  unreadMessages: number;
  
-  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService, private messageService: MessageService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {   
-    this.loadMessages();
   }
 
   login() {
-    this.accountService.login(this.model).subscribe(() => {
-      this.loadMessages();
-      this.router.navigateByUrl("/members");      
+    this.accountService.login(this.model).subscribe(() => {  
+      this.router.navigateByUrl("/members");          
     }, () => {
       this.toastr.error("Invalid credentials", "Login Failed");
     })
@@ -37,10 +32,10 @@ export class NavComponent implements OnInit {
     this.router.navigateByUrl("/");
   }
 
-  loadMessages() {
-    this.messageService.getMessages(1, 5, this.container).subscribe(response => {
-      this.messages = response.result;   
-      this.unreadMessages = this.messages.length;
-    })
-  }
+  // loadMessages() {
+  //   this.messageService.getMessages(1, 5, this.container).subscribe(response => {
+  //     this.messages = response.result;   
+  //     this.unreadMessages = this.messages.length;
+  //   })
+  // }
 }
